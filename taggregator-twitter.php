72ba3bec2af1d0a3dcce54ba49d5ccf0997d8056
George Stephanis <george@stephanis.info>
2013-09-15 00:32:54 -0400

    Add in fields for the Access Token and Access Token Secrets as well.

--- taggregator-twitter.php
+++ taggregator-twitter.php
@@ -25,6 +25,14 @@ class Taggregator_Twitter {
 				if ( defined( 'TAGGREGATOR_TWITTER_CONSUMER_SECRET' ) ) {
 					return TAGGREGATOR_TWITTER_CONSUMER_SECRET;
 				}
+			case 'twitter_access_token' :
+				if ( defined( 'TAGGREGATOR_TWITTER_ACCESS_TOKEN' ) ) {
+					return TAGGREGATOR_TWITTER_ACCESS_SECRET;
+				}
+			case 'twitter_access_token_secret' :
+				if ( defined( 'TAGGREGATOR_TWITTER_ACCESS_TOKEN_SECRET' ) ) {
+					return TAGGREGATOR_TWITTER_ACCESS_TOKEN_SECRET;
+				}
 		}
 
 		return Taggregator::get_option( $key );
@@ -50,6 +58,26 @@ class Taggregator_Twitter {
 				'taggregator'
 			);
 		}
+
+		if ( ! defined( 'TAGGREGATOR_TWITTER_ACCESS_TOKEN' ) ) {
+			add_settings_field(
+				'taggregator_twitter_access_token',
+				sprintf( '<label for="taggregator_twitter_access_token">%1$s</label>', __( 'Twitter Access Token', 'taggregator' ) ),
+				array( $this, 'taggregator_twitter_access_token_cb' ),
+				'discussion',
+				'taggregator'
+			);
+		}
+
+		if ( ! defined( 'TAGGREGATOR_TWITTER_ACCESS_TOKEN_SECRET' ) ) {
+			add_settings_field(
+				'taggregator_twitter_access_token_secret',
+				sprintf( '<label for="taggregator_twitter_access_token_secret">%1$s</label>', __( 'Twitter Access Token Secret', 'taggregator' ) ),
+				array( $this, 'taggregator_twitter_access_token_secret_cb' ),
+				'discussion',
+				'taggregator'
+			);
+		}
 	}
 
 	function taggregator_twitter_consumer_key_cb() {
@@ -65,12 +93,30 @@ class Taggregator_Twitter {
 		<?php
 	}
 
+	function taggregator_twitter_access_token_cb() {
+		?>
+		<input class="regular-text code" type="text" id="taggregator_twitter_access_token" name="taggregator_options[twitter_access_token]" value="<?php echo esc_attr( $this->get_option( 'twitter_access_token' ) ); ?>" />
+		<?php
+	}
+
+	function taggregator_twitter_access_token_secret_cb() {
+		?>
+		<input class="regular-text code" type="text" id="taggregator_twitter_access_token_secret" name="taggregator_options[twitter_access_token_secret]" value="<?php echo esc_attr( $this->get_option( 'twitter_access_token_secret' ) ); ?>" />
+		<?php
+	}
+
 	function sanitize_options( $options ) {
 		if ( isset( $options['twitter_consumer_key'] ) ) {
 			$options['twitter_consumer_key'] = sanitize_text_field( $options['twitter_consumer_key'] );
 		}
 		if ( isset( $options['twitter_consumer_secret'] ) ) {
 			$options['twitter_consumer_secret'] = sanitize_text_field( $options['twitter_consumer_secret'] );
+		}
+		if ( isset( $options['twitter_access_token'] ) ) {
+			$options['twitter_access_token'] = sanitize_text_field( $options['twitter_access_token'] );
+		}
+		if ( isset( $options['twitter_access_token_secret'] ) ) {
+			$options['twitter_access_token_secret'] = sanitize_text_field( $options['twitter_access_token_secret'] );
 		}
 		return $options;
 	}
